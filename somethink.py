7b5a10dfd37dc1ae5c9b65a495d7bb0f9fb87743
Daniel Reynard <daniel.reynard.k@gmail.com>
2021-09-05 17:53:29 +0700

    new1

--- somethink.py
+++ somethink.py
@@ -6,4 +6,8 @@ if __name__ == '__main__':
 def mutate_string(string, position, character):
   return
 
-
+if __name__ == '__main__':
+  s = raw_input()
+  i, c = raw_input().split()
+  s_new = mutate_string(s, int(i), c)
+  print s_new
